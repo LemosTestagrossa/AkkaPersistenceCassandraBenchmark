e58028f17b75e500e7cb9d1ae52f03673738745c
miguelemosreverte <miguelemosreverte@gmail.com>
2020-09-05 23:35:57 -0300

    kubectl

--- .github/actions/kubectl/entrypoint.sh
+++ .github/actions/kubectl/entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+
+aws eks update-kubeconfig
+
+
+# confirm it worked by listing your pods
+kubectl get pods --all-namespaces
+
+# or list your helm deployments
+helm ls --all-namespaces
+
+
 kubectl get all
 
 sh -c "$*"
