9d6978df1b7a6d9b3ceefba693d45a6c1459a929
miguelemosreverte <miguelemosreverte@gmail.com>
2020-09-05 23:49:33 -0300

    kubectl

--- .github/actions/kubectl/entrypoint.sh
+++ .github/actions/kubectl/entrypoint.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
+set -xe
 
-aws eks update-kubeconfig
+sh -c "aws configure set aws_access_key_id ${aws_access_key_id}"
+sh -c "aws configure set aws_secret_access_key ${aws_secret_access_key}"
+sh -c "aws configure set region ${aws_region}"
 
+# Extract the base64 encoded config data and write this to the KUBECONFIG
 
-# confirm it worked by listing your pods
-kubectl get pods --all-namespaces
+# Write a ~/.kube/config file, using the AWS CLI to fetch the necessary parameters
+mkdir -p ~/.kube
+echo "$KUBE_CONFIG_DATA" | base64 --decode > ~/.kube/config
 
-# or list your helm deployments
-helm ls --all-namespaces
-
+# export KUBECONFIG=/tmp/config
 
 kubectl get all
 
