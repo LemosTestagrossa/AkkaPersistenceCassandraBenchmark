60875628d67c48adadba11c91646b570fa09bea1
miguelemosreverte <miguelemosreverte@gmail.com>
2020-09-06 01:05:02 -0300

    kubectl

--- .github/actions/kubectl/entrypoint.sh
+++ .github/actions/kubectl/entrypoint.sh
@@ -7,7 +7,7 @@ sh -c "aws configure set aws_secret_access_key ${aws_secret_access_key}"
 sh -c "aws configure set region ${aws_region}"
 
 # Extract the base64 encoded config data and write this to the KUBECONFIG
-echo "$kube_confg_data" | base64 --decode > /tmp/config
+echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
 
 
