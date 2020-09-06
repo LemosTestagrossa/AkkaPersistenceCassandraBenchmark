c947a0eea65c5af68d52b06dfc3d02d674830f92
miguelemosreverte <miguelemosreverte@gmail.com>
2020-09-06 00:55:48 -0300

    kubectl

--- .github/actions/kubectl/entrypoint.sh
+++ .github/actions/kubectl/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 
-export ADMIN_POD=$(kubectl get pod -l app=ubuntu -o jsonpath='{.items[0].metadata.name}')
-echo "ADMIN_POD: $ADMIN_POD"
+export ADMIN_POD_NAME=$(kubectl get pod -l app=ubuntu -o jsonpath='{.items[0].metadata.name}')
+echo "ADMIN_POD_NAME: $ADMIN_POD"
 
-sh -c "kubectl exec -i $(ADMIN_POD) -- bash -c $*"
+sh -c "kubectl exec -i $(ADMIN_POD_NAME) -- bash -c $*"
 
